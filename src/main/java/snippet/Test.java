87e6b7b0d5189bedb91ffcac568c1f8e43b1ece3
UNOSHE0516 <hiro0516jms@gmail.com>
2025-01-13 00:12:23 +0900

    DBConnectorのurlを変更したが変わらず

--- src/main/java/snippet/Test.java
+++ src/main/java/snippet/Test.java
@@ -3,11 +3,8 @@ package snippet;
 public class Test {
 	public static void main(String[] args) {
 			// TODO 自動生成されたメソッド・スタブ
-			
 			Snippet dao = new Snippet(); 
 				dao.select("taro", "123"); 
-				
 				}
-			
 		}
 
